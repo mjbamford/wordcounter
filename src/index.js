a38e7d840fbe206da9cd22cb5cf9423e754250c0
Michael Bamford <michael@michaelbamford.com>
2021-12-13 11:47:01 +1100

    Add fake request

--- src/index.js
+++ src/index.js
@@ -1,26 +1,74 @@
 (function() {
-    let _state = { text: '' }
+    const targetWordCount = 10;
+    const IDLE = 'IDLE';
+    const SUCCESS = 'SUCCESS';
+    const FAILURE = 'FAILURE';
+    const WAITING = 'WAITING';
+
+    let _state = { text: '', saveStatus: IDLE }
 
     function setState(newState) {
         _state = Object.assign({}, _state, newState);
         render();
     }
 
+    function countWords(text) {
+        let matches;
+        return (text && (matches = text.match(/\w+/g))) ? matches.length : 0
+    }
+
+    function makeFakeRequest(text, success, failure) {
+        setTimeout(function() {
+            if (Math.random() > 0.5) {
+                success();
+            } else {
+                failure();
+            }
+        }, 1500);
+    }
+
     function render() {
-        const text  = _state.text
+        const { text, saveStatus } = _state;
+
         const editor = document.getElementById("editor");
         const counter = document.getElementById("counter");
+        const progressBar = document.getElementById("progress");
+        const alertBox = document.getElementById("alertBox");
+
+        const wordCount = countWords(text);
+        const progressValue = wordCount / targetWordCount;
+
+        let message = '';
+        if (saveStatus === FAILURE) {
+            message = 'Failed :('
+        } else if (saveStatus === SUCCESS) {
+            message = 'Success!';
+        } else if (saveStatus === WAITING) {
+            message = 'Saving...'
+        }
 
         editor.value = text;
-        counter.innerHTML = "Word Count: 666";
+        counter.innerHTML = `Word Count: ${wordCount}`;
+        progressBar.value = progressValue;
+        alertBox.textContent = message;
     }
 
     function initialize() {
         const editor = document.getElementById("editor");
+        const saveBtn = document.getElementById('saveButton')
         editor.addEventListener('input', function(event) {
-            const text = event.target.value;
+            let text = event.target.value;
             text = text.toUpperCase();
             setState({ text: text });
+        });
+
+        saveBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            setState({ saveStatus: WAITING });
+            makeFakeRequest(_state.text,
+                () => setState({ saveStatus: SUCCESS }),
+                () => setState({ saveStatus: FAILURE })
+            );
         });
     }
 
